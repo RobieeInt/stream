8ee25ad1d10476ba47a5a3ec1364684dc8f2e630
RobieeInt <74244073+RobieeInt@users.noreply.github.com>
2022-09-22 00:15:38 +0700

    url sidebar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,83 +1,84 @@
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="{{ asset("adminlte/dist/img/laravel.png") }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">STREAM</span>
+    <a href="{{ route('admin.dashboard') }}" class="brand-link">
+        <img src="{{ asset('adminlte/dist/img/laravel.png') }}" alt="AdminLTE Logo" class="brand-image elevation-3"
+            style="opacity: .8">
+        <span class="brand-text font-weight-light">STREAM</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="{{ asset("adminlte/dist/img/user2-160x160.jpg") }}" class="img-circle elevation-2" alt="User Image">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="{{ asset('adminlte/dist/img/avatar4.png') }}" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="#" class="d-block">Super Admin</a>
+            </div>
         </div>
-        <div class="info">
-          <a href="#" class="d-block">Super Admin</a>
-        </div>
-      </div>
 
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <li class="nav-item">
-            <a href="pages/widgets.html" class="nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Dashboard
-              </p>
-            </a>
-          </li>
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <li class="nav-item">
+                    <a href="{{ route('admin.dashboard') }}" class="nav-link">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>
+                            Dashboard
+                        </p>
+                    </a>
+                </li>
 
-          <li class="nav-item">
-            <a href="{{ route('admin.transaction.index') }}" class="nav-link">
-              <i class="nav-icon fas fa-shopping-cart"></i>
-              <p>
-                Transactions
-              </p>
-            </a>
-          </li>
+                <li class="nav-item">
+                    <a href="{{ route('admin.transaction.index') }}" class="nav-link">
+                        <i class="nav-icon fas fa-shopping-cart"></i>
+                        <p>
+                            Transactions
+                        </p>
+                    </a>
+                </li>
 
-          <li class="nav-item">
-            <a href="{{ route('admin.movie.index')}}" class="nav-link">
-              <i class="nav-icon fas fa-video"></i>
-              <p>
-                Movies
-              </p>
-            </a>
-          </li>
+                <li class="nav-item">
+                    <a href="{{ route('admin.movie.index')}}" class="nav-link">
+                        <i class="nav-icon fas fa-video"></i>
+                        <p>
+                            Movies
+                        </p>
+                    </a>
+                </li>
 
-          <li class="nav-item">
-            <a href="{{ route('admin.blog.index')}}" class="nav-link">
-              <i class="nav-icon fas fa-blog"></i>
-              <p>
-                Blog
-              </p>
-            </a>
-          </li>
+                <li class="nav-item">
+                    <a href="{{ route('admin.blog.index')}}" class="nav-link">
+                        <i class="nav-icon fas fa-blog"></i>
+                        <p>
+                            Blog
+                        </p>
+                    </a>
+                </li>
 
-          <li class="nav-item">
-            <a href="{{ route('admin.package.index') }}" class="nav-link">
-              <i class="nav-icon fas fa-cubes"></i>
-              <p>
-                Package
-              </p>
-            </a>
-          </li>
+                <li class="nav-item">
+                    <a href="{{ route('admin.package.index') }}" class="nav-link">
+                        <i class="nav-icon fas fa-cubes"></i>
+                        <p>
+                            Package
+                        </p>
+                    </a>
+                </li>
 
-          <li class="nav-item">
-            <a href="{{ route('admin.user.index') }}" class="nav-link">
-              <i class="nav-icon fas fa-users"></i>
-              <p>
-                User
-              </p>
-            </a>
-          </li>
+                <li class="nav-item">
+                    <a href="{{ route('admin.user.index') }}" class="nav-link">
+                        <i class="nav-icon fas fa-users"></i>
+                        <p>
+                            User
+                        </p>
+                    </a>
+                </li>
 
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
     </div>
     <!-- /.sidebar -->
-  </aside>
+</aside>
